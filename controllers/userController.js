e9b468b5e721db55d1b5cbf3958b53ed40d29503
uhfm <uhfm@fresnou.org>
2023-04-11 11:37:04 -0700

    slide 36

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+const {User, Role} = require("../models")
+const md5 = require('md5')
+
+module.exports.renderRegistrationForm =  async function(req, res){
+    const roles = await Role.findAll()
+    console.log(roles);
+    res.render('users/register', {roles})
+}
+
+module.exports.register =  async function(req,res){
+    await User.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: md5(req.body.password),
+        role_id: req.body.role
+    })
+    res.redirect('/login')
+}
+
+module.exports.login = async function(req, res){
+    res.render('users/login')
+}
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const articleController = require('../controllers/articleController')
 const commentController = require('../controllers/commentController')
+const userController = require('../controllers/userController')
  router.get('/', function(req, res){
    res.redirect('/article')
  })
@@ -24,4 +25,10 @@ router.get('/article/:articleId/delete', articleController.deleteArticle)
 router.post('/article/:articleId/comment/create', commentController.createComment)
 
 router.post('/comment/:commentId/reply/create', commentController.addRelpy)
+
+
+router.get('/register', userController.renderRegistrationForm)
+router.post('/register', userController.register)
+
+router.get('/login', userController.login)
 module.exports = router;
